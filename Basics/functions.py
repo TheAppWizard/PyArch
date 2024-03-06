d3302ac1565fa9952bf655f4ba87a644616040e8
TheAppWizard <theappwizard2408@gmail.com>
2024-03-06 09:33:18 +0530

    Basic Day 2

--- Basics/functions.py
+++ Basics/functions.py
@@ -1,3 +1,34 @@
+# ---------------------
+# Basic Function
+# ---------------------
+
 def greet_user():
     print("Hello Python")
 greet_user()
+
+# ---------------------
+# Passing an argument
+# ---------------------
+
+def greet_username(username):
+    print("Hello, " + username + "!")
+greet_username("Shreyas") # Hello, Shreyas!
+
+# ---------------------
+# Default argument
+# ---------------------
+
+def make_pizza(topping='bacon'):
+    print("Have a " + topping + " pizza!")
+make_pizza() #Have a bacon pizza!
+make_pizza('pepperoni') #Have a pepperoni pizza!
+
+
+# ---------------------
+# Return
+# ---------------------
+
+def add_numbers(x, y):
+    return x + y
+sum = add_numbers(5, 3)
+print(sum) 
